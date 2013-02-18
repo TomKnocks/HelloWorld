c1e75666984048f191af6764281bbac16477b5ae
THoppeTF <t.hoppe@dpcom.de>
2013-02-18 16:04:43 +0100

    Tadaaaa 2
    Signed-off-by: THoppeTF <t.hoppe@dpcom.de>

---
+++ src/main/java/de/tom/HelloGit.java
@@ -0,0 +1,36 @@
+
+/******************************************************************************
+ * Deutsche Post Com GmbH
+ * Tulpenfeld 9
+ * 53113 Bonn
+ * Germany
+ *
+ * (c) Copyright 2013 by Deutsche Post Com GmbH
+ * ALL RIGHTS RESERVED
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * $Id: HelloGit.java,v 1.0 18.02.2013 user Exp $
+ ******************************************************************************/
+package de.tom;
+/**
+ * HelloGit declaration
+ *
+ * @version $Id: HelloGit.java,v 1.0 18.02.2013 16:02:09 user Exp $
+ */
+public class HelloGit {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("TröööT");
+  }
+
+}
+
+
+/******************************************************************************
+ * $Log: HelloGit.java,v $
+ *
+ ******************************************************************************/
